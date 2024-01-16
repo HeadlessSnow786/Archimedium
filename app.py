cd049af79abaac1ccb9c76e266b754a3ed1df969
Aditya Kolekar <128189377+HeadlessSnow786@users.noreply.github.com>
2024-01-16 09:39:28 -0800

    Cleaned Up Project Files

--- app.py
+++ app.py
@@ -6,13 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/unitSix', methods=['GET', 'POST'])
-def unitSix():
+@app.route('/inputTest', methods=['GET', 'POST'])
+def inputTest():
     if request.method == 'POST':
             data = request.form['prompt']
-            return render_template('unitSix.html', data=data)
+            return render_template('inputTest.html', data=data)
     else:
-        return render_template('unitSix.html')
+        return render_template('inputTest.html')
+    
+@app.route('/latexTest')
+def latexTest():
+    return render_template('latexTest.html')
     
 if __name__ == '__main__':
     app.run(debug=True)
